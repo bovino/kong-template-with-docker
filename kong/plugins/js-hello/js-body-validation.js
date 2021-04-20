df5c35dbc2258ff153b1d61134bc5b3a83375828
Marcelo Bezerra <marcelo.junior@luizalabs.com>
2021-04-20 18:01:56 -0300

    adding a sample plugin that prints body content and perform some validation on it

---
+++ kong/plugins/js-hello/js-body-validation.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let proc = require("process")
+
+// This is an example plugin that prints body content and apply some validation on it
+
+class KongPlugin {
+  constructor(config) {
+    this.config = config
+  }
+
+  async access(kong) {
+
+    kong.log.inspect(await kong.request.getBody("application/json"));
+    let body = await kong.request.getBody("application/json");
+
+    try {
+      if(body[0].content === undefined || body[0].content.text === undefined || body[0].content.text === ""){
+        kong.log.debug('VALIDATION - Text is required!');
+        await kong.response.exit(400, "Text is required.");
+      }
+      kong.log.info(' Body "content.text" ::: ' + body[0].content.text);
+
+    } catch(err){
+        kong.log.debug('Error printing content.text from request body ::: ' + err);
+        await kong.response.exit(400, "Body mal-formed.");
+    }
+
+    kong.log.info('END of access phase!');
+  }
+}
+
+module.exports = {
+  Plugin: KongPlugin,
+  Schema: [],
+  Version: '0.1.0',
+  Priority: 0,
+}
