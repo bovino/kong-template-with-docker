4dda46a6b0cb4e49d28321f89bf87ad3c317487b
bovino <FalaSerio@2016>
2021-04-21 13:39:45 -0300

    updating error output on validation to use  JSON format

--- kong/plugins/js-hello/js-body-validation.js
+++ kong/plugins/js-hello/js-body-validation.js
@@ -1,5 +1,4 @@
 'use strict';
-
 let proc = require("process")
 
 // This is an example plugin that prints body content and apply some validation on it
@@ -13,17 +12,30 @@ class KongPlugin {
 
     let body = await kong.request.getBody("application/json");
     kong.log.inspect(body);
+    const validationObj = {};
 
     try {
       if(body[0].content === undefined || body[0].content.text === undefined || body[0].content.text === ""){
-        kong.log.debug('VALIDATION - Text is required!');
-        await kong.response.exit(400, "Text is required.");
+         kong.log.debug('VALIDATION - Text is required!');
+         validationObj.field = "content.text";
+         validationObj.message = "Text is required.";
+         await kong.response.exit(400, validationObj);
+       }
+       kong.log.info(' Body "content.text" ::: ' + body[0].content.text);
+
+      /* if(body[0].content === undefined || body[0].content.username === undefined || body[0].content.username === ""){
+        kong.log.debug('VALIDATION - username is required!');
+        validationObj.field = "content.username";
+        validationObj.message = "Username is required.";
+        await kong.response.exit(400, validationObj);
       }
-      kong.log.info(' Body "content.text" ::: ' + body[0].content.text);
+      kong.log.info(' Body "content.username" ::: ' + body[0].content.username); */
 
     } catch(err){
         kong.log.debug('Error printing content.text from request body ::: ' + err);
-        await kong.response.exit(400, "Body mal-formed.");
+        validationObj.field = "body";
+        validationObj.message = "Body mal-formed.";
+        await kong.response.exit(400, validationObj);
     }
 
     kong.log.info('END of access phase!');
